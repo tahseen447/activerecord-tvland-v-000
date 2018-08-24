b5b0865625c7e8244518222f4564a17ebfe8e58e
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 00:21:36 +0000

    automatically backed up by learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,7 +7,7 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    characters.collect do |character|
+    self.characters.collect do |character|
       "#{character.name} - #{character.show.name}"
     end
   end
